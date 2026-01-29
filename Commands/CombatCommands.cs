fa51c3321a595898a347d819d35a15fd033c30e6
61807112CTC <61807112@student.muskegonisd.org>
2026-01-29 12:02:06 -0500

    Fixed some mistakes with the combat commands

--- Commands/CombatCommands.cs
+++ Commands/CombatCommands.cs
@@ -6,6 +6,19 @@ using RPGFramework.Workflows;
 
 namespace RPGFramework.Commands
 {
+    internal class CombatCommands
+    {
+        public static List<ICommand> GetAllCommands()
+        {
+            return
+            [
+                new ConsiderCommand(),
+                new ComabtStatusCommand(),
+                new StartCombatCommand(),
+                new CombatAdminControlsCommand()
+            ];
+        }
+    }
 
     internal class ConsiderCommand : ICommand
     {
@@ -91,129 +104,128 @@ namespace RPGFramework.Commands
 
             }
         }
+    }
 
-        internal class ComabtStatusCommand : ICommand
+    internal class ComabtStatusCommand : ICommand
+    {
+
+        public string Name => "combatstatus";
+        public IEnumerable<string> Aliases => new List<string> { "/combatstatus", "/cs" };
+        public bool Execute(Character character, List<string> parameters)
         {
+            if (character is not Player player)
+                return false;
 
-            public string Name => "combatstatus";
-            public IEnumerable<string> Aliases => new List<string> { "/combatstatus", "/cs" };
-            public bool Execute(Character character, List<string> parameters)
+
+            if (Utility.CheckPermission(player, PlayerRole.Admin))
             {
-                if (character is not Player player)
-                    return false;
-
-
-                if (Utility.CheckPermission(player, PlayerRole.Admin))
+                switch (parameters.Count)
                 {
-                    switch (parameters.Count)
-                    {
-                        case 1:
-                            player.WriteLine("Must choose a character to see the combat status");
-                            return false;
+                    case 1:
+                        player.WriteLine("Must choose a character to see the combat status");
+                        return false;
 
-                        case 2:
+                    case 2:
 
-                            return ShowCombatStatus(player, parameters);
+                        return ShowCombatStatus(player, parameters);
 
-                        default:
+                    default:
 
-                            player.WriteLine("Must choose a character to see the combat status");
-                            return false;
-                    }
-                }
-                else
-                {
-                    player.WriteLine("You do not have permission to use this command.");
-                    return false;
-                }
-
-
-            }
-            private bool ShowCombatStatus(Player p, List<string> parameters)
-            {
-                CombatWorkflow currentCombat = null;
-                foreach (CombatWorkflow combat in GameState.Instance.Combats)
-                {
-                    if (combat.Combatants.Contains(p))
-                    {
-                        currentCombat = combat;
-                        break;
-                    }
-                }
-                if (currentCombat != null)
-                {
-                    p.WriteLine("Combatants:");
-                    foreach (Character c in currentCombat.Combatants)
-                    {
-                        p.WriteLine($"{c.Name} - HP: {c.Health}/{c.MaxHealth}");
-                    }
-                    return true;
-                }
-                else
-                {
-                    return false;
-
+                        player.WriteLine("Must choose a character to see the combat status");
+                        return false;
                 }
             }
+            else
+            {
+                player.WriteLine("You do not have permission to use this command.");
+                return false;
+            }
+
+
         }
-
-        internal class CombatCommands
+        private bool ShowCombatStatus(Player p, List<string> parameters)
         {
-            internal class StartCombatCommand : ICommand
+            CombatWorkflow currentCombat = null;
+            foreach (CombatWorkflow combat in GameState.Instance.Combats)
             {
-                public string Name => "attack";
-                public IEnumerable<string> Aliases => new List<string> { "/attack", "/a" };
-                public bool Execute(Character character, List<string> parameters)
+                if (combat.Combatants.Contains(p))
                 {
-
-                    List<string> attackableNonPlayers = new List<string>();
-
-                    foreach (NonPlayer npc in character.GetRoom().GetNonPlayers())
-                    {
-                        attackableNonPlayers.Add(npc.Name);
-                    }
-
-                    List<string> attackablePlayers = new List<string>();
-
-                    foreach (Player p in character.GetRoom().GetPlayers())
-                    {
-                        attackablePlayers.Add(p.Name);
-                    }
-                    if (parameters.Count < 2)
-                    {
-                        if (character is Player player)
-                        {
-
-                            player.WriteLine("Attackable NonPlayers");
-                            foreach (string s in attackableNonPlayers)
-                            {
-                                player.WriteLine(s);
-                            }
-                            player.WriteLine("Attackable Players");
-                            foreach (string s in attackablePlayers)
-                            {
-                                player.WriteLine(s);
-                            }
-
-
-                        }
-                    }
-                    if (parameters.Count == 2)
-                    {
-                        if (attackablePlayers.Contains(parameters[1]) || attackableNonPlayers.Contains(parameters[1]))
-                        {
-                            Character enemy = character.GetRoom().GetCharacters().Find(Character => Character.Name == parameters[1]);
-                            CombatWorkflow.CreateCombat(character, enemy);
-                            return true;
-                        }
-                    }
-                    return false;
+                    currentCombat = combat;
+                    break;
                 }
+            }
+            if (currentCombat != null)
+            {
+                p.WriteLine("Combatants:");
+                foreach (Character c in currentCombat.Combatants)
+                {
+                    p.WriteLine($"{c.Name} - HP: {c.Health}/{c.MaxHealth}");
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+
             }
         }
     }
 
-    internal class CombatAdminControls : ICommand
+
+    internal class StartCombatCommand : ICommand
+    {
+        public string Name => "attack";
+        public IEnumerable<string> Aliases => new List<string> { "/attack", "/a" };
+        public bool Execute(Character character, List<string> parameters)
+        {
+
+            List<string> attackableNonPlayers = new List<string>();
+
+            foreach (NonPlayer npc in character.GetRoom().GetNonPlayers())
+            {
+                attackableNonPlayers.Add(npc.Name);
+            }
+
+            List<string> attackablePlayers = new List<string>();
+
+            foreach (Player p in character.GetRoom().GetPlayers())
+            {
+                attackablePlayers.Add(p.Name);
+            }
+            if (parameters.Count < 2)
+            {
+                if (character is Player player)
+                {
+
+                    player.WriteLine("Attackable NonPlayers");
+                    foreach (string s in attackableNonPlayers)
+                    {
+                        player.WriteLine(s);
+                    }
+                    player.WriteLine("Attackable Players");
+                    foreach (string s in attackablePlayers)
+                    {
+                        player.WriteLine(s);
+                    }
+
+
+                }
+            }
+            if (parameters.Count == 2)
+            {
+                if (attackablePlayers.Contains(parameters[1]) || attackableNonPlayers.Contains(parameters[1]))
+                {
+                    Character enemy = character.GetRoom().GetCharacters().Find(Character => Character.Name == parameters[1]);
+                    CombatWorkflow.CreateCombat(character, enemy);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+    internal class CombatAdminControlsCommand : ICommand
     {
         public void AdminStartCombatUntargeted(Player player)
         {
