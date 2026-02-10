341a427530388f71da6839d1389707ad6865cc07
TylerBoyYes <61803051@fruitportstudents.net>
2026-02-10 12:35:54 -0500

    item create update

--- Items/Item.cs
+++ Items/Item.cs
@@ -8,9 +8,12 @@ namespace RPGFramework
         public string DisplayText { get; set; } = ""; // How it appears when in a room
         public bool IsDroppable { get; set; } // Can the item be dropped
 
+        public int Durability { get; set; } //how durable the weapon is 
+
         public bool IsDropped { get; set; } = false; // Not sure what this is for? (Mr. Brown)
         public bool IsGettable { get; set; } // Can the item be picked up
         public bool IsPerishable { get; set; } = false;
+        public string WeaponType { get; set; } = "";
 
         public int Level { get; set; } = 0;
         public string Name { get; set; } = "";
